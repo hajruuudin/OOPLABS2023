abca1c72848c9f41783469b466ff94c9d3e3c2cf
Hajrudin Imamovic <hajruuudin@gmail.com>
2023-10-22 18:34:59 +0200

    COMMITED WEEK 3

---
+++ WEEK3/src/main/java/org/example/Status.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum Status {
+    TO_DO, IN_PROGRESS, COMPLETED, CANCELLED;
+}
---
+++ WEEK3/src/main/java/org/example/TaskItem.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class TaskItem {
+    private int taskId;
+    private String taskDescription;
+    private Status status;
+
+    public TaskItem(int id, String desc, Status status){
+        this.taskId = id;
+        this.taskDescription = desc;
+        this.status = status;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
